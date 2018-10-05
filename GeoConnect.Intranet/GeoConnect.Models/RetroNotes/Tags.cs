871b6f7252f950ad841d3f2a25775752aa34cc0e
SharedOps <manikanta.mundru@gmail.com>
2018-10-05 12:49:13 -0700

    Poco classes added

---
+++ GeoConnect.Intranet/GeoConnect.Models/RetroNotes/Tags.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoConnect.Models.RetroNotes
+{
+
+    public class Tags
+    {
+        public Tags()
+        {
+            this.DateCreated = DateTime.Now;
+            this.CreatedBy   = System.Environment.UserName;
+        }
+        public int Id { get; set; }
+        public string TagName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+
+}
