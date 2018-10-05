a6332402f957e6dd45c4e455ac61afecd371c1e9
SharedOps <manikanta.mundru@gmail.com>
2018-10-05 07:51:09 -0700

    Projecy scafolding added

---
+++ GeoConnect.Intranet/GeoConnect.DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoConnect.DAL
+{
+    public class Class1
+    {
+    }
+}
